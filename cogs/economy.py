436af3e9a4ca503e71f8ea4827f6374b4199a8f5
Jon Huber <hubermjonathan@gmail.com>
2020-06-18 17:17:08 -0700

    updated eco algorithm

--- cogs/economy.py
+++ cogs/economy.py
@@ -18,11 +18,12 @@ class Economy(commands.Cog):
 
     async def send_balance(self, payload):
         balance = self.redis.hget(payload.member.id, 'points').decode('utf-8')
-        await payload.member.send(f'your current balance is **{balance} dining dollars** 💵')
+        await payload.member.send(f'your current balance is **{float(balance)/5} dining dollars** 💵')
 
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
-        if member.bot or (before.channel is not None and after.channel is not None):
+        self.redis.hset(member.id, 'points', 0)
+        if member.bot:
             return
 
         if before.channel is None:
@@ -31,13 +32,21 @@ class Economy(commands.Cog):
         elif after.channel is None:
             start_time = self.redis.hget(member.id, 'start_time').decode('utf-8')
             old_points = float(self.redis.hget(member.id, 'points').decode('utf-8'))
-            new_points = math.floor((time() - float(start_time)) / 5)
+            new_points = math.floor(time() - float(start_time))
             self.redis.hset(member.id, 'points', old_points + new_points)
+
+        else:
+            start_time = self.redis.hget(member.id, 'start_time').decode('utf-8')
+            old_points = float(self.redis.hget(member.id, 'points').decode('utf-8'))
+            new_points = math.floor(time() - float(start_time))
+            if new_points > 5:
+                self.redis.hset(member.id, 'points', old_points + new_points)
+                self.redis.hset(member.id, 'start_time', time())
 
     @commands.command()
     async def mute(self, ctx, member: discord.Member):
         balance = self.redis.hget(ctx.author.id, 'points').decode('utf-8')
-        if float(balance) < 20:
+        if float(balance)/5 < 20:
             await ctx.message.add_reaction('👎')
             return
 
