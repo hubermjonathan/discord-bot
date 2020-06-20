c08e2aea75fc9b1a5e5aab34b4dfe0cc0713941c
Jon Huber <hubermjonathan@gmail.com>
2020-06-19 19:19:29 -0700

    added eco reset and total points

--- cogs/economy.py
+++ cogs/economy.py
@@ -12,7 +12,6 @@ def setup(bot):
 class Economy(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        globals.redis.delete(self.bot.owner_id)
 
     @tasks.loop(minutes=10)
     async def count_points(self):
@@ -22,11 +21,12 @@ class Economy(commands.Cog):
                 continue
 
             for m in vc.members:
-                if m.bot or guild.get_role(globals.default_role_id) in m.roles or m.id != self.bot.owner_id:
+                if m.bot or guild.get_role(globals.default_role_id) in m.roles:
                     continue
 
                 if globals.redis.hget(m.id, 'points') is None:
                     globals.redis.hset(m.id, 'points', 0)
+                    globals.redis.hset(m.id, 'total_points', 0)
                     globals.redis.hset(m.id, 'last_mute', time())
                     globals.redis.hset(m.id, 'last_rmute', time())
                     globals.redis.hset(m.id, 'last_muted', time())
@@ -34,6 +34,9 @@ class Economy(commands.Cog):
                 old_points = float(globals.redis.hget(m.id, 'points').decode('utf-8'))
                 new_points = old_points + (82.5/(60/10))
                 globals.redis.hset(m.id, 'points', new_points)
+                old_total_points = float(globals.redis.hget(m.id, 'total_points').decode('utf-8'))
+                new_total_points = old_total_points + (82.5/(60/10))
+                globals.redis.hset(m.id, 'total_points', new_total_points)
 
                 if globals.redis.hget(m.id, 'points_message_id') is None:
                     await m.create_dm()
@@ -47,3 +50,16 @@ class Economy(commands.Cog):
     @count_points.before_loop
     async def before_loop(self):
         await self.bot.wait_until_ready()
+
+    @commands.group(aliases=['e'])
+    async def economy(self, ctx):
+        if ctx.invoked_subcommand is None:
+            await ctx.message.add_reaction('❔')
+
+    @economy.command()
+    async def reset(self, ctx):
+        guild = self.bot.get_guild(globals.guild_id)
+        for m in guild.get_role(globals.promoted_role_id).members:
+            globals.redis.delete(m.id)
+
+        await ctx.message.add_reaction('👍')
