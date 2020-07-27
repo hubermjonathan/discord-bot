519bd50069ba159ed66d85e39a223f2c2862a0a8
Jon Huber <hubermjonathan@gmail.com>
2020-07-26 17:47:42 -0700

    fixed welcome, added intent

--- src/main/java/com/hubermjonathan/mitch/Mitch.java
+++ src/main/java/com/hubermjonathan/mitch/Mitch.java
@@ -4,12 +4,13 @@ package com.hubermjonathan.mitch;
 import com.hubermjonathan.mitch.welcome.WelcomeEvents;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 
 import javax.security.auth.login.LoginException;
 
 public class Mitch {
     public static void main(String[] args) throws LoginException {
-        JDA api = JDABuilder.createDefault(System.getenv("TOKEN")).build();
+        JDA api = JDABuilder.createDefault(System.getenv("TOKEN")).enableIntents(GatewayIntent.GUILD_MEMBERS).build();
         api.addEventListener(new Dispatcher());
         api.addEventListener(new WelcomeEvents());
     }
