dd6985515cc9f5581c39bdc146e38adac92ffa5f
Jon Huber <hubermjonathan@gmail.com>
2020-12-13 16:00:11 -0800

    fixed last commit

--- src/main/java/com/hubermjonathan/mitch/Constants.java
+++ src/main/java/com/hubermjonathan/mitch/Constants.java
@@ -3,8 +3,8 @@ package com.hubermjonathan.mitch;
 import redis.clients.jedis.Jedis;
 
 public class Constants {
-    public static final String BOT_CHANNEL_ID = "787488862619697182";
-    public static final String DEFAULT_ROLE_ID = "702469536825344040";
+    public static final String BOT_CHANNEL_ID = "702469536825344040";
+    public static final String DEFAULT_ROLE_ID = "701162007981981771";
 
     public static final String REDIS_URL = System.getenv("REDIS_URL");
     public static final Jedis JEDIS = new Jedis(REDIS_URL);
