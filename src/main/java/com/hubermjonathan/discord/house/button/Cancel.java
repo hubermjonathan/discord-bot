95d2d217ef634204850b51b7164ef06c2c35c463
Jon Huber <hubermjonathan@gmail.com>
2022-01-13 22:18:11 -0800

    fixed cancel bug

--- src/main/java/com/hubermjonathan/discord/house/button/Cancel.java
+++ src/main/java/com/hubermjonathan/discord/house/button/Cancel.java
@@ -9,6 +9,10 @@ public class Cancel extends GuestButton {
 
     @Override
     public void execute() throws Exception {
+        if (!getEvent().getMember().equals(getEvent().getMessage().getMentionedMembers().get(1))) {
+            throw new Exception();
+        }
+
         getEvent().deferEdit().queue();
         getEvent().getMessage().delete().queue();
     }
