52ca6aac6e349b308f97915f5a6adf3b1f872d2a
Jon Huber <hubermjonathan@gmail.com>
2022-01-27 01:08:30 -0800

    poke jon irl

--- src/main/java/com/hubermjonathan/discord/house/button/Poke.java
+++ src/main/java/com/hubermjonathan/discord/house/button/Poke.java
@@ -5,6 +5,8 @@ import com.hubermjonathan.discord.house.model.ResidentButton;
 import com.hubermjonathan.discord.house.player.PlayerManager;
 import net.dv8tion.jda.api.entities.Emoji;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class Poke extends ResidentButton {
     public Poke(String name) {
@@ -25,8 +27,11 @@ public class Poke extends ResidentButton {
 
         if (getEvent().getTargetMember().getVoiceState().inAudioChannel()) {
             PlayerManager.getInstance().loadAndPlay(getEvent().getGuild(), getEvent().getTargetMember().getVoiceState().getChannel(), Constants.POKE_SOUND_FEMALE);
+        } else if (getEvent().getTargetMember().getId().equals("196141424318611457")) {
+            HttpURLConnection connection = (HttpURLConnection) new URL("http://76.217.54.67:5000/poke").openConnection();
+            connection.getInputStream();
         }
 
-        return " you poked " + getEvent().getTargetMember().getAsMention() + "! " + Emoji.fromUnicode(Constants.POKE).getName();
+        return "you poked " + getEvent().getTargetMember().getAsMention() + "! " + Emoji.fromUnicode(Constants.POKE).getName();
     }
 }
