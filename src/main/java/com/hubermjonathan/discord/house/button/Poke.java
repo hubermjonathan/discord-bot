42a23ea924eac14b809be1fcbffcfa738c2e6562
Jon Huber <hubermjonathan@gmail.com>
2022-01-27 01:30:06 -0800

    bot no longer gets angry when poking jon offline

--- src/main/java/com/hubermjonathan/discord/house/button/Poke.java
+++ src/main/java/com/hubermjonathan/discord/house/button/Poke.java
@@ -5,6 +5,8 @@ import com.hubermjonathan.discord.house.model.ResidentButton;
 import com.hubermjonathan.discord.house.player.PlayerManager;
 import net.dv8tion.jda.api.entities.Emoji;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
+
+import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -28,8 +30,11 @@ public class Poke extends ResidentButton {
         if (getEvent().getTargetMember().getVoiceState().inAudioChannel()) {
             PlayerManager.getInstance().loadAndPlay(getEvent().getGuild(), getEvent().getTargetMember().getVoiceState().getChannel(), Constants.POKE_SOUND_FEMALE);
         } else if (getEvent().getTargetMember().getId().equals("196141424318611457")) {
-            HttpURLConnection connection = (HttpURLConnection) new URL("http://76.217.54.67:5000/poke").openConnection();
-            connection.getInputStream();
+            try {
+                HttpURLConnection connection = (HttpURLConnection) new URL("http://76.217.54.67:5000/poke").openConnection();
+                connection.getInputStream();
+            } catch (Exception e) {
+            }
         }
 
         return "you poked " + getEvent().getTargetMember().getAsMention() + "! " + Emoji.fromUnicode(Constants.POKE).getName();
