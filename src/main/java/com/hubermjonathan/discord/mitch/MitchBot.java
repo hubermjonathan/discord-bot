0177419ab9e3ca22719a61a002842f09da947bb7
hubermjonathan <hubermjonathan@gmail.com>
2023-01-25 04:40:16 -0800

    mitch is even meaner

--- src/main/java/com/hubermjonathan/discord/mitch/MitchBot.java
+++ src/main/java/com/hubermjonathan/discord/mitch/MitchBot.java
@@ -3,9 +3,12 @@ package com.hubermjonathan.discord.mitch;
 import com.hubermjonathan.discord.mitch.events.*;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.ChunkingFilter;
 import net.dv8tion.jda.api.utils.MemberCachePolicy;
+
+import java.util.Timer;
 
 public class MitchBot {
     public static void run(final String token) throws  InterruptedException {
@@ -20,5 +23,11 @@ public class MitchBot {
                 .build();
 
         jda.awaitReady();
+
+        for (Guild guild : jda.getGuilds()) {
+            Timer timer = new Timer();
+
+            timer.schedule(new KickStrangers(guild), 1000 * 60 * 60 * 24, 1000 * 60 * 60 * 12);
+        }
     }
 }
