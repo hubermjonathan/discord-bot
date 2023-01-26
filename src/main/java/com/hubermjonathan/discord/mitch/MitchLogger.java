32a2fce37492542205eb03ade1612dbf46eef8f1
hubermjonathan <hubermjonathan@gmail.com>
2023-01-25 16:03:32 -0800

    mitch took a quick trip to greenwich

--- src/main/java/com/hubermjonathan/discord/mitch/MitchLogger.java
+++ src/main/java/com/hubermjonathan/discord/mitch/MitchLogger.java
@@ -5,7 +5,9 @@ import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.utils.messages.MessageCreateBuilder;
 
 import java.text.SimpleDateFormat;
+import java.time.ZoneId;
 import java.util.Date;
+import java.util.TimeZone;
 
 public class MitchLogger {
     public static void log(final User user, final String title, final String message) {
@@ -16,6 +18,7 @@ public class MitchLogger {
         embedBuilder.setTitle(title);
         embedBuilder.setDescription(message);
         embedBuilder.setColor(0xcfb991);
+        dateFormatter.setTimeZone(TimeZone.getTimeZone(ZoneId.of(ZoneId.SHORT_IDS.get("PST"))));
         embedBuilder.setFooter(dateFormatter.format(new Date()));
         messageBuilder.setEmbeds(embedBuilder.build());
         user
