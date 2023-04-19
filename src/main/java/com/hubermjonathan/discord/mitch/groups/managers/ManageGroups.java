ac3a150c0218a193923a3f4a9dd9f05bbe9bc37e
Jon Huber <hubermjonathan@gmail.com>
2023-04-19 15:13:21 -0700

    mitch includes the necessary people

--- src/main/java/com/hubermjonathan/discord/mitch/groups/managers/ManageGroups.java
+++ src/main/java/com/hubermjonathan/discord/mitch/groups/managers/ManageGroups.java
@@ -3,6 +3,7 @@ package com.hubermjonathan.discord.mitch.groups.managers;
 import com.hubermjonathan.discord.common.models.Manager;
 import com.hubermjonathan.discord.mitch.groups.Util;
 import com.hubermjonathan.discord.mitch.groups.buttons.JoinOrLeaveGroup;
+import net.dv8tion.jda.api.entities.channel.ChannelType;
 import net.dv8tion.jda.api.events.channel.ChannelCreateEvent;
 import net.dv8tion.jda.api.events.channel.ChannelDeleteEvent;
 import net.dv8tion.jda.api.events.channel.update.ChannelUpdateNameEvent;
@@ -18,6 +19,10 @@ public class ManageGroups extends Manager {
 
     @Override
     public void onChannelCreate(@NotNull ChannelCreateEvent event) {
+        if (!event.isFromType(ChannelType.TEXT)) {
+            return;
+        }
+
         event.getJDA().addEventListener(
                 new JoinOrLeaveGroup(event.getChannel().asTextChannel().getId()));
 
@@ -26,11 +31,19 @@ public class ManageGroups extends Manager {
 
     @Override
     public void onChannelDelete(@NotNull ChannelDeleteEvent event) {
+        if (!event.isFromType(ChannelType.TEXT)) {
+            return;
+        }
+
         Util.updateGroupsMessage(event);
     }
 
     @Override
     public void onChannelUpdateName(@NotNull ChannelUpdateNameEvent event) {
+        if (!event.isFromType(ChannelType.TEXT)) {
+            return;
+        }
+
         Util.updateGroupsMessage(event);
     }
 }
--- src/main/java/com/hubermjonathan/discord/mitch/groups/managers/ManageThreads.java
+++ src/main/java/com/hubermjonathan/discord/mitch/groups/managers/ManageThreads.java
@@ -1,8 +1,13 @@
 package com.hubermjonathan.discord.mitch.groups.managers;
 
 import com.hubermjonathan.discord.common.models.Manager;
+import net.dv8tion.jda.api.entities.MessageReaction;
 import net.dv8tion.jda.api.entities.channel.ChannelType;
 import net.dv8tion.jda.api.events.channel.ChannelCreateEvent;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ManageThreads extends Manager {
     @Override
@@ -17,5 +22,31 @@ public class ManageThreads extends Manager {
                 .getManager()
                 .setName(String.format("\uD83D\uDCAC %s (tbd)", event.getChannel().getName().toLowerCase()))
                 .queue();
+
+        List<MessageReaction> reactions = event
+                .getChannel()
+                .asThreadChannel()
+                .retrieveParentMessage()
+                .complete()
+                .getReactions();
+
+        if (reactions.size() > 0) {
+            List<String> reactionMentions = new ArrayList();
+
+            for (MessageReaction reaction : reactions) {
+                reactionMentions.add(
+                        String.join(
+                                " ",
+                                reaction.retrieveUsers().stream().map(user -> user.getAsMention()).collect(Collectors.toList())
+                        )
+                );
+            }
+
+            event
+                    .getChannel()
+                    .asThreadChannel()
+                    .sendMessage(String.join(" ", reactionMentions))
+                    .queue();
+        }
     }
 }
