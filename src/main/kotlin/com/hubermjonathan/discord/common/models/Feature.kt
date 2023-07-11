d5e0056647089d97303a7c1f8c17c58b756aecd9
Drew Davis <drewdavis@hey.com>
2023-07-11 01:31:17 -0400

    Make features manage their own tasks and commands

--- src/main/kotlin/com/hubermjonathan/discord/common/models/Feature.kt
+++ src/main/kotlin/com/hubermjonathan/discord/common/models/Feature.kt
@@ -4,29 +4,32 @@ import com.hubermjonathan.discord.common.DiscordLogger
 import net.dv8tion.jda.api.JDA
 import java.util.Timer
 
-abstract class Feature(jda: JDA, name: String, icon: String) {
+abstract class Feature(private val jda: JDA, private val name: String, icon: String) {
     abstract val buttons: List<Button>
     abstract val commands: List<Command>
     abstract val managers: List<Manager>
     abstract val tasks: List<Task>
     protected val context = FeatureContext(jda, logger = DiscordLogger(jda, name, icon), name, icon)
 
-    fun load(jda: JDA) {
+    fun load() {
+        val timer = Timer()
         val listenerAdapters = buttons + commands + managers
 
+        commands.forEach { command ->
+            jda.guilds.forEach { guild ->
+                guild.upsertCommand(command.commandData)
+            }
+        }
         listenerAdapters.forEach {
             jda.addEventListener(it)
         }
         managers.forEach {
             it.whenLoaded()
         }
-    }
-
-    fun startTasks() {
-        val timer = Timer()
-
         tasks.forEach {
             timer.schedule(it, it.delay.toMillis(), it.schedule.toMillis())
         }
     }
+
+    override fun toString() = this.name
 }
